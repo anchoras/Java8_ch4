4cbbff6ad0e6a8b85441f641cdfd68d4bc1e2405
anchoras <anchoras2@gmail.com>
2021-01-25 21:56:50 +0300

    added the very basics of class constractor

--- VehicleDemo.java
+++ VehicleDemo.java
@@ -130,29 +130,23 @@ class Vehicle {
     double fuelneeded(int milesNeeded) {
         return (double) milesNeeded/milepergallon;
     }
+
+    Vehicle(int p, int f, int m) {
+        passengers = p;
+        fuelcap = f;
+        milepergallon = m;
+    }
 }
 
 class VehicleDemo {
     public static void main (String args[]) {
-        Vehicle minivan = new Vehicle();
-        Vehicle sportcar = new Vehicle();
+        Vehicle minivan = new Vehicle(7, 16, 21);
+        Vehicle sportcar = new Vehicle(2, 14, 12);
         double gallons;
 
         int distance = 252;
 
-        minivan.passengers = 7;
-        minivan.fuelcap = 16;
-        minivan.milepergallon = 21;
 
-        sportcar.passengers = 2;
-        sportcar.fuelcap = 14;
-        sportcar.milepergallon = 12;
-
-        //rangeMinivan = minivan.range();
-        //rangeSportcar = sportcar.range();
-
-        //System.out.println("Minivan can transport " + minivan.passengers + " passangers for " + rangeMinivan + " miles");
-        //System.out.println("Sportcar can transport " + sportcar.passengers + " passangers for " + rangeSportcar + " miles");
 
         System.out.println("For minivan\'s way for " + distance + " miles the voluem of " + minivan.fuelneeded(distance) + " gallons of fuel needed");
         System.out.println("For sportcar\'s way for " + distance + " miles the voluem of " + sportcar.fuelneeded(distance) + " gallons of fuel needed");
